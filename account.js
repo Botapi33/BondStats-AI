545fb5ab58f6eaa832a7834c3d821f94ddfec402
Botapi33 <71960375+Botapi33@users.noreply.github.com>
2026-08-11 17:25:58 +0200

    Update account.js

--- account.js
+++ account.js
@@ -56,16 +56,16 @@
        ======================================================== */
 
     const db = window.supabase.createClient(
-      SUPABASE_URL,
-      SUPABASE_PUBLISHABLE_KEY,
-      {
-        auth: {
-          persistSession: true,
-          autoRefreshToken: true,
-          detectSessionInUrl: true
-        }
-      }
-    );
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
+);
 
 
     /* ========================================================
